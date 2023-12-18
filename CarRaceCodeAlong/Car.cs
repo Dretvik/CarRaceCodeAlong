98474ed53157435426b901cf04a6db14fd5ec0c2
Ellie Marie <orix@hotmail.no>
2023-12-18 12:25:28 +0100

    Add project files.

---
+++ CarRaceCodeAlong/Car.cs
@@ -0,0 +1,71 @@
+﻿
+namespace CarRaceCodeAlong
+{
+    internal class Car
+    {
+        internal string Name { get; set; }
+        internal int MeterS { get; set; }
+        internal int Distance { get; set; }
+
+
+        internal Car(string name, int meterS, int distance)
+        {
+            Name = name;
+            MeterS = meterS;
+            Distance = distance;
+        }
+
+        internal void DriveCar()
+        {
+            Console.Clear();
+
+            while (true)
+            {
+                Distance += MeterS;
+
+                if (Distance <= 500)
+                {
+                    MeterS += 10;
+
+                }
+                else
+                {
+                    MeterS -= 10;
+                }
+
+                if (Distance >= 1000)
+                {
+                    Console.WriteLine($"\nYou are going {MeterS}Meters/s.\nYou have driven {Distance} Meters ");
+
+                    return;
+                }
+
+                Thread.Sleep(1000);
+                Console.WriteLine($"\nYou are going {MeterS}Meters/s.\nYou have driven {Distance} Meters ");
+            }
+
+        }
+        internal async Task RaceTwoCarsAsync()
+        {
+            var speed = new Random();
+
+            while (true)
+            {
+                Distance += MeterS;
+
+                MeterS = speed.Next(60, 200);
+
+                Console.WriteLine($"\n{Name} is driving at {MeterS} meters/s and is at {Distance} meters.");
+
+                if (Distance >= 10000)
+                {
+                    Console.WriteLine($"Car {Name} is done!");
+                    return;
+                }
+
+                await Task.Delay(200);
+            }
+
+        }
+    }
+}
---
+++ CarRaceCodeAlong/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace CarRaceCodeAlong
+{
+    internal class Program
+    {
+        internal Car car;
+        internal List<Car> cars;
+
+
+        internal Program()
+        {
+            car = new Car("Lone Car", 10, 0);
+
+            cars = new List<Car>
+            {
+               new Car("Car One", 10, 0),
+               new Car("Car Two", 10, 0),
+            };
+        }
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Run();
+        }
+
+        internal void Run()
+        {
+            Console.WriteLine("Hello, and welcome to Car race console app");
+
+            while (true)
+            {
+                Console.WriteLine("1. Drive one car \n2. Race two cars \n0. Exit");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        car.DriveCar();
+                        break;
+                    case "2":
+                        RaceAllCarsAsync();
+                        break;
+                    case "0":
+                        return;
+                    default:
+                        Console.WriteLine("Invalid input, try again");
+                        break;
+                }
+            }
+        }
+
+        internal async Task RaceAllCarsAsync()
+        {
+            List<Task> raceTasks = new List<Task>();
+
+            foreach (var singleCar in cars)
+            {
+                raceTasks.Add(singleCar.RaceTwoCarsAsync());
+            }
+
+            await Task.WhenAny(raceTasks);
+        }
+    }
+}
